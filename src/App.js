aecce4a983d13417667861932f3629162ebe4736
MrGultekin <gultekin.de@gmail.com>
2022-04-20 00:17:13 +0200

    import { BrowserRouter, Routes, Route } from "react-router-dom;

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import './App.css';
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout/Checkout";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Hero/>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/checkout"
+                        element={
+                            <>
+                                <Header />
+                                <Checkout />
+                            </>
+                        }
+                    />
+
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Header />
+                                <Hero />
+                            </>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <Router>
+//                 <Routes>
+//
+//                     <Route path="/checkout">
+//                         <Header/>
+//                         <Checkout/>
+//                     </Route>
+//
+//                     <Route path="/">
+//                         <Header/>
+//                         <Hero/>
+//                     </Route>
+//
+//                 </Routes>
+//             </Router>
+//
+//         </div>
+//     );
+// }
 
 export default App;
